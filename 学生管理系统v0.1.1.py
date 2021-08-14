05384d0a8ce0fd3dc4fdc54a6fe63bbd3b24bb84
2865172449aaazzz <2865172449aaazzz@sina.com>
2021-08-14 22:26:37 +0800

    命名不规范修复2108142226

--- 学生管理系统v0.1.1.py
+++ 学生管理系统v0.1.1.py
@@ -1,19 +1,16 @@
-name = {'liming': [100, 65, 84]}
-jishu1 = 0
+Results dictionary = {'liming': [100, 65, 84]}
+count = 0
 
-
-
-print('欢迎使用学生管理系统v0.1.1\n©Copyright2021杨一朔.AllRightsReserved\n按回车键开始使用')
-input()
+print('欢迎使用学生管理系统v0.1.1\n©Copyright2021杨一朔.AllRightsReserved')
 
 while True:
 
-    for i in name:
+    for i in Results dictionary:
         allgrade = 0
-    if len(name[i]) == 3:
-        for q in name[i]:
+    if len(Results dictionary[i]) == 3:
+        for q in Results dictionary[i]:
             allgrade += int(q)
-        name[i].append(allgrade)
+        Results dictionary[i].append(allgrade)
 
     
     print('填写所需选项的序号:')
@@ -28,7 +25,7 @@ while True:
 
     if a == 1:
         print('(注：输出的顺序为:\n姓名 语文成绩 数学成绩 英语成绩 总分)')
-        print(name)
+        print(Results dictionary)
         f = input('输入‘F’回到主菜单')
         if f == 'F':
             f = '' 
@@ -36,18 +33,18 @@ while True:
 
     if (a == 2):
         while True:
-            newname = str(input('请输入新学生或需要修改的学生名字：'))
+            newResults dictionary = str(input('请输入新学生或需要修改的学生名字：'))
             newgrade_Chinese = input('请输入Ta的语文成绩：')
             newgrade_Mach = input('请输入Ta的数学成绩：')
             newgrade_English = input('请输入Ta的英语成绩：')
             allnewgrade = [newgrade_Chinese, newgrade_Mach, newgrade_English]
-            name.update({newname: allnewgrade})
-            for i in name:
+            Results dictionary.update({newResults dictionary: allnewgrade})
+            for i in Results dictionary:
                 allgrade = 0
-                if len(name[i]) == 3:
-                    for q in name[i]:
+                if len(Results dictionary[i]) == 3:
+                    for q in Results dictionary[i]:
                         allgrade += int(q)
-                    name[i].append(allgrade)
+                    Results dictionary[i].append(allgrade)
             f = input('继续添加输入‘T’，回到主菜单输入‘F’\n请输入：')
             if (f == 'F'):
                 break
@@ -57,9 +54,9 @@ while True:
 
     if (a == 3):
         while True:
-            delname = input('请输入需要删除的学生名字:\n')
-            if (delname in name):
-                name.pop(delname)
+            delResults dictionary = input('请输入需要删除的学生名字:\n')
+            if (delResults dictionary in Results dictionary):
+                Results dictionary.pop(delResults dictionary)
                 f = input('已删除,继续删除输入‘T’，返回主菜单输入‘F’\n请输入：')
                 if f == 'F':
                     break
@@ -74,16 +71,16 @@ while True:
 
     if (a == 4):
         while True:
-            for i in name:
+            for i in Results dictionary:
                 allgrade = 0
-                if len(name[i]) == 3:
-                    for q in name[i]:
+                if len(Results dictionary[i]) == 3:
+                    for q in Results dictionary[i]:
                         allgrade += int(q)
-                    name[i].append(allgrade)
-            findname = input('请输入需要查找成绩的学生名字:\n')
-            if (findname in name):
+                    Results dictionary[i].append(allgrade)
+            findResults dictionary = input('请输入需要查找成绩的学生名字:\n')
+            if (findResults dictionary in Results dictionary):
                 
-                print(name[findname])
+                print(Results dictionary[findResults dictionary])
                 f = input('已完成，继续查询输入‘T’，回到主菜单输入‘F’\n请输入：')
                 if f == 'F':
                     break
@@ -99,23 +96,23 @@ while True:
 
     if (a == 5):
         #预留
-        jishu1 += 1 
-        if jishu1 <= 3:
+        count += 1 
+        if count <= 3:
             print('===================================')
             print('此功能尚未开发，期待后续新版本更新')
             print('===================================')
             continue
-        if jishu1 >= 3 <= 4:
+        if count >= 3 <= 4:
             print('===================================')
             print('都说了没开发呢，就别按了')
             print('===================================')
             continue
-        if jishu1 >= 5 <= 6:
+        if count >= 5 <= 6:
             print('===================================')
             print('你再按也™没有，憋按了')
             print('===================================')
             continue
-        if jishu1 >= 7:
+        if count >= 7:
             print('===================================')
             print('再见吧')
             print('===================================')
